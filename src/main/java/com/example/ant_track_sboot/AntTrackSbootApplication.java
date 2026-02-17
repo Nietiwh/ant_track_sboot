2bb18bffe05084074e6e33d836f2f91128544a59
Maricela <correo_ochoamaricela22@gmail.com>
2026-02-17 16:51:53 -0500

    feat: creando api en spring boot

---
+++ src/main/java/com/example/ant_track_sboot/AntTrackSbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ant_track_sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntTrackSbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntTrackSbootApplication.class, args);
+	}
+
+}
