6b5a52392ff63cf0a760b33e341e815622a6afd5
julian <julenposper@outlook.com>
2026-02-28 14:25:50 -0500

    feat se crea tabla categoria

--- src/main/java/com/example/ant_track_sboot/modelo/Categoria.java
+++ src/main/java/com/example/ant_track_sboot/modelo/Categoria.java
@@ -1,19 +1,142 @@
 package com.example.ant_track_sboot.modelo;
 
-public enum Categoria {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
-    COMIDA,
+import java.time.LocalDateTime;
 
-    TRANSPORTE,
+@Entity
+@Table(name="categorias")
 
-    ROPA,
+public class Categoria {
 
-    ANTOJOS,
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
 
-    SUSCRIPCIONES,
+   @Column(nullable = false, length = 100)
+   private String nombre;
 
-    HOGAR,
+   @Column(length = 250)
+   private String descripcion;
 
-    OTROS
+   @Column(nullable = false)
+   private double presupuestoMaximoMensual;
+
+   @Column(nullable = false)
+   private double gastoMensual;
+
+   @Column(nullable = false)
+   private boolean esNecesaria;
+
+   @Column(nullable = false)
+   private boolean alertaActiva;
+
+   @Column(nullable = false)
+   private int prioridad;
+
+   @Column(nullable = false)
+   private boolean activa;
+
+   @Column(nullable = false)
+   private LocalDateTime fechaCreacion;
+
+
+   public Categoria(){
+
+   }
+
+
+   public Categoria(String nombre, String descripcion, double presupuestoMaximoMensual, boolean esNecesaria, Integer prioridad) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.presupuestoMaximoMensual = presupuestoMaximoMensual;
+    this.esNecesaria = esNecesaria;
+    this.prioridad = 1;
+    this.gastoMensual = 0;
+    this.alertaActiva = false;
+    this.activa = true;
+    this.fechaCreacion = LocalDateTime.now();
+   }
+
+
+   public Long getId() {
+    return id;
+   }
+
+
+   public String getNombre() {
+    return nombre;
+   }
+
+
+   public String getDescripcion() {
+    return descripcion;
+   }
+
+
+   public double getPresupuestoMaximoMensual() {
+    return presupuestoMaximoMensual;
+   }
+
+
+   public double getGastoMensual() {
+    return gastoMensual;
+   }
+
+
+   public boolean isEsNecesaria() {
+    return esNecesaria;
+   }
+
+
+   public boolean isAlertaActiva() {
+    return alertaActiva;
+   }
+
+
+   public Integer getPrioridad() {
+    return prioridad;
+   }
+
+
+   public boolean isActiva() {
+    return activa;
+   }
+
+
+   public LocalDateTime getFechaCreacion() {
+    return fechaCreacion;
+   }
+
+
+   public void setNombre(String nombre) {
+    this.nombre = nombre;
+   }
+
+
+   public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+   }
+
+
+   public void setPresupuestoMaximoMensual(double presupuestoMaximoMensual) {
+    this.presupuestoMaximoMensual = presupuestoMaximoMensual;
+   }
+
+   
+   
+   
+
+
+
+
+
+
+
 
 }
