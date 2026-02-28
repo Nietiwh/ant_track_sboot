22d951f0db9d8ab9808b4f6e9294fff59ebfab37
julian <julenposper@outlook.com>
2026-02-28 17:46:15 -0500

    comentarios

--- src/main/java/com/example/ant_track_sboot/modelo/Categoria.java
+++ src/main/java/com/example/ant_track_sboot/modelo/Categoria.java
@@ -1,5 +1,7 @@
+//Le digo a Java dónde vive este archivo dentro del proyecto.
 package com.example.ant_track_sboot.modelo;
 
+//trayendo las herramientas que vamos a usar
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
@@ -9,48 +11,51 @@ import jakarta.persistence.Column;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name="categorias")
+
+@Entity //le dice a spring:esta clase es una tabla en la base de datos
+
+@Table(name="categorias")//le dice como se llama la tabla para que no ponga el nombre por defecto
 
 public class Categoria {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Id // este campo es la llave primaria, el identificador único de cada fila
+   @GeneratedValue(strategy = GenerationType.IDENTITY)//el número lo genera automáticamente la base de datos
    private Long id;
 
-   @Column(nullable = false, length = 100)
+   @Column(nullable = false, length = 100)//configura cómo es esa columna en la BD, el nullable que el campo sea obligatorio
    private String nombre;
 
    @Column(length = 250)
    private String descripcion;
 
-   @Column(nullable = false)
+   @Column(nullable = false)// el usuario define cuanto quiere gastar, en decimal 
    private double presupuestoMaximoMensual;
 
-   @Column(nullable = false)
+   @Column(nullable = false)// lo que el usuario vaya gastandp . lo comenzamos con 0.0 y el sistema lo actualiza
    private double gastoMensual;
 
-   @Column(nullable = false)
+   @Column(nullable = false)// para pregunatr al ususario si la compra es necesaria o no
    private boolean esNecesaria;
 
    @Column(nullable = false)
-   private boolean alertaActiva;
+   private boolean alertaActiva;// para que seactive en caso de que el gasto este llegqando al presupuesto, el clinete no lo toca
 
-   @Column(nullable = false)
+   @Column(nullable = false)//un numero para asignar que tan importante es la categoria, lo asignas el sisitema, comienza en 1
    private int prioridad;
 
-   @Column(nullable = false)
+   @Column(nullable = false)//sirve para desactivar una categoria , sin que afecte a la base de datos, es como esconder el registro
    private boolean activa;
 
-   @Column(nullable = false)
+   @Column(nullable = false)//gaurada fecha y hora de cuando se crea
    private LocalDateTime fechaCreacion;
 
-
+    //constructor vacio, jpa lo usa para crear internamente los objetos
    public Categoria(){
 
    }
 
-
+   //Este es el constructor que se usa cuando el usuario crea una categoría. Solo recibe los 4 datos que el usuario escribe.
+   //  Todo lo demás lo asigna el sistema automáticamente
    public Categoria(String nombre, String descripcion, double presupuestoMaximoMensual, boolean esNecesaria, Integer prioridad) {
     this.nombre = nombre;
     this.descripcion = descripcion;
@@ -63,7 +68,7 @@ public class Categoria {
     this.fechaCreacion = LocalDateTime.now();
    }
 
-
+   //creamos los get , la ventana para poder ver el valor del atributo fuera de la clase
    public Long getId() {
     return id;
    }
@@ -113,7 +118,7 @@ public class Categoria {
     return fechaCreacion;
    }
 
-
+   //los set, para modificar datos desde afuera, solo tenemos 3 porque son los que cambia el usuario,despues de crear la categoria
    public void setNombre(String nombre) {
     this.nombre = nombre;
    }
