c3874b531955fa070d5af33c54cf9dfba30d4dec
VELASQUEZ <aurejr11@gmail.com>
2026-03-01 18:25:43 -0500

    Feat: Cambios en tabla comercio

--- src/main/java/com/example/ant_track_sboot/modelo/Comercio.java
+++ src/main/java/com/example/ant_track_sboot/modelo/Comercio.java
@@ -2,7 +2,7 @@ package com.example.ant_track_sboot.modelo;
 
 import java.time.LocalDate;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,80 +19,117 @@ public class Comercio {
 
 @Id //anotacion id  para indicar que es la clave primaria
 @GeneratedValue(strategy = GenerationType.IDENTITY) // anotacion para indicar que el valor se genera automaticamente por la base de datos
+
 private Integer id;
+
+@Column(nullable = false, length = 30)
 private String nit;
-private String nombre_comercio;
+
+@Column(nullable = false, length = 100)
+private String nombreComercio;
+
+@Column(nullable = false, length = 100)
 private String actividad;
+
+@Column(nullable = false, length = 20)
 private String contacto;
+
+@Column(nullable = false, length = 150)
 private String direccion;
+
+@Column(nullable = false, length = 100)
 private String barrio;
+
+@Column(nullable = false, length = 100)
 private String ciudad;
-private LocalDate fecha_creacion;
-private String horario_atencion;
+
+@Column(nullable = false)
+private LocalDate fechaCreacion;
+
+@Column(nullable = false, length = 100)
+private String horarioAtencion;
 
 
 /*Creacion de get and set de cada dato */
-public Comercio() {
-}
+
 public Integer getId() {
     return id;
 }
+
 public void setId(Integer id) {
     this.id = id;
 }
+
 public String getNit() {
     return nit;
 }
+
 public void setNit(String nit) {
     this.nit = nit;
 }
-public String getNombre_comercio() {
-    return nombre_comercio;
+
+public String getNombreComercio() {
+    return nombreComercio;
 }
-public void setNombre_comercio(String nombre_comercio) {
-    this.nombre_comercio = nombre_comercio;
+
+public void setNombreComercio(String nombreComercio) {
+    this.nombreComercio = nombreComercio;
 }
+
 public String getActividad() {
     return actividad;
 }
+
 public void setActividad(String actividad) {
     this.actividad = actividad;
 }
+
 public String getContacto() {
     return contacto;
 }
+
 public void setContacto(String contacto) {
     this.contacto = contacto;
 }
+
 public String getDireccion() {
     return direccion;
 }
+
 public void setDireccion(String direccion) {
     this.direccion = direccion;
 }
+
 public String getBarrio() {
     return barrio;
 }
+
 public void setBarrio(String barrio) {
     this.barrio = barrio;
 }
+
 public String getCiudad() {
     return ciudad;
 }
+
 public void setCiudad(String ciudad) {
     this.ciudad = ciudad;
 }
-public LocalDate getFecha_creacion() {
-    return fecha_creacion;
+
+public LocalDate getFechaCreacion() {
+    return fechaCreacion;
 }
-public void setFecha_creacion(LocalDate fecha_creacion) {
-    this.fecha_creacion = fecha_creacion;
+
+public void setFechaCreacion(LocalDate fechaCreacion) {
+    this.fechaCreacion = fechaCreacion;
 }
-public String getHorario_atencion() {
-    return horario_atencion;
+
+public String getHorarioAtencion() {
+    return horarioAtencion;
 }
-public void setHorario_atencion(String horario_atencion) {
-    this.horario_atencion = horario_atencion;
+
+public void setHorarioAtencion(String horarioAtencion) {
+    this.horarioAtencion = horarioAtencion;
 }
 
 
