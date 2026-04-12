53e9a73b74cd9c7863dd07a9cbe9eb38625753a9
zanti82 <zanti82@gmail.com>
2026-04-12 12:29:01 -0500

    fix: Organiznado gasto servicio, corriegiendo errores de nombres de repositorios

--- src/main/java/com/example/ant_track_sboot/repositorio/IGastoRepositorio.java
+++ src/main/java/com/example/ant_track_sboot/repositorio/IGastoRepositorio.java
@@ -4,10 +4,32 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.ant_track_sboot.modelo.Gasto;
+import java.util.List;
+
 
 @Repository
 public interface IGastoRepositorio extends JpaRepository<Gasto, Long >{
 
-    //implemanta todo el  crud
+    //implemanta todo el  crud clasico
+
+    //buscar por nombre de gasto que contengan(esperamos una lista)
+
+    List<Gasto> findByDescripcion(String descripcion);
+
+    
+
+    // estoe  spara buscar por una sola cosa Optional<Gasto> findByDocumento(String documento);
+    
+    //buscar por nombre de gasto que contengan(esperamos una lista)
+
+    List<Gasto> findByDescripcionContaining(String descripcion);
+
+    
+
+
+    
+
+    //buscar por cantidad
+
     
 }
